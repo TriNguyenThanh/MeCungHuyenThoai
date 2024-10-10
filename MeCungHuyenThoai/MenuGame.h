0bb95ae9b9339571739847b665f90e7e97763bae
MinhTu <minhtu06041@gmail.com>
2024-10-10 16:03:53 +0700

    minhTu fix

---
+++ MeCungHuyenThoai/MenuGame.h
@@ -0,0 +1,13 @@
+#ifndef MENUGAME_H_
+#define MENUGAME_H_
+
+#include "BaseObject.h"
+#include "function.h"
+
+
+
+namespace SDLMenuGame
+{
+	int ShowMenu(SDL_Surface* des, TTF_Font* font);
+}
+#endif // 0
