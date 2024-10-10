16e7cd4c3ed96c0baa7db9ec9baaff6c80876051
MinhTu <minhtu06041@gmail.com>
2024-10-10 16:07:23 +0700

    minhTU update map

--- MeCungHuyenThoai/FpsControl.cpp
+++ MeCungHuyenThoai/FpsControl.cpp
@@ -1,15 +1,39 @@
 #include "FpsControl.h"
 
 Timer::Timer() {
-	tick = 0;
+	start_tick = 0;
+	pause_tick = 0;
+	game_pause = false;
+	game_start = true;
 }
 Timer::~Timer() {
 
 }
 int Timer::get_tick() {
 
-	return tick;
+	return start_tick;
 }
-void Timer::start() {
-	tick = SDL_GetTicks();
+void Timer::startGame() {
+	game_pause = false;
+	game_start = true;
+	start_tick = SDL_GetTicks();
+}
+void Timer::pauseGame() {
+	if (game_start == true && game_pause == false)
+	game_pause = true;
+	pause_tick = SDL_GetTicks() - start_tick;
+}
+void Timer::unpauseGame()
+{
+	if (game_pause == true)
+	{
+		game_pause = false;
+	}
+}
+void Timer::stopGame()
+{
+	start_tick = 0;
+	pause_tick = 0;
+	game_pause = false;
+	game_start = false;
 }
