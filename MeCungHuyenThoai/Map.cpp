9e0f27cd72378877829e153ef2f66009a65ce719
MinhTu <minhtu06041@gmail.com>
2024-11-11 13:46:43 +0700

    Minh Tú up load menu

---
+++ MeCungHuyenThoai/Map.cpp
@@ -0,0 +1,23 @@
+#include "Menu.h"
+
+Menu::Menu()
+{
+	menu_texture = nullptr;
+	bg_texture = nullptr;
+	run_bg_texture = nullptr;
+	title_texture = nullptr;
+	button1_texture = nullptr;
+	button2_texture = nullptr;
+
+	font1 = font2 = nullptr;
+
+	title_text = button1_text = button2_text = "none";
+	title_size = button_size = 0;
+
+	run_bg_rect.x = run_bg_rect.y = run_bg_rect.w = run_bg_rect.h = 0;
+	title.x = title.y = title.w = title.h = 0;
+	button1.x = button1.y = button1.w = button1.h = 0;
+	button2.x = button2.y = button2.w = button2.h = 0;
+
+	button1_is_hover = button2_is_hover = false;
+}
